8af7955e3adb4b9b94d3e605acc1bcad90cf216f
Justin Byun <klofseattle@hotmail.com>
2021-06-10 12:33:43 -0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sampleBlogCards: [
+      {blogTitle: ' Card #1', blogCoverPhoto: 'stock-1', blogDate:'May 1 2021'},
+      {blogTitle: ' Card #2', blogCoverPhoto: 'stock-2', blogDate:'May 1 2021'},
+      {blogTitle: ' Card #3', blogCoverPhoto: 'stock-3', blogDate:'May 1 2021'},
+      {blogTitle: ' Card #4', blogCoverPhoto: 'stock-4', blogDate:'May 1 2021'},
+    ],
+    editPost: null,
+    user: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileInitials: null,
+  },
+  mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    },
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id;
+      state.profileEmail = doc.data().email;
+      state.profileFirstName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUsername = doc.data().username;
+      // console.log(state.profileId);
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFirstName.match(/(\b\S)?/g).join("") + state.profileLastName.match(/(\b\S)?/g).join("");
+    },
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+  },
+  actions: {
+    async getCurrentUser({commit}) {
+      const dataBase = await db.collection("users").doc(firebase.auth().currentUser.uid);
+      const dbResults = await dataBase.get();
+      commit("setProfileInfo", dbResults);
+      commit("setProfileInitials");
+      // console.log('>>> dbResults', dbResults);
+    }
+  },
+  modules: {
+  }
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,122 @@
+/* eslint-disable vue/valid-v-for */
+<template>
+  <div class="home">
+    <BlogPost v-if='!user' :post="welcomeScreen"/>
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogsPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View more recent blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post='post' v-for='(post, index) in sampleBlogCards' :key='index' />
+        </div>
+      </div>
+    </div>
+    <div v-if='!user' class="updates">
+      <div class="container">
+        <h2>Register for your free account, meaning free updates!</h2>
+        <router-link class='router-button' to='#'>
+          Register for Fireblogs!
+          <Arrow class='arrow arrow-light' />
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogPost from '@/components/BlogPost.vue';
+import BlogCard from '@/components/BlogCard.vue';
+import Arrow from "../assets/Icons/arrow-right-light.svg";
+
+export default {
+  name: "Home",
+  components: {
+    BlogPost,
+    BlogCard,
+    Arrow
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome',
+        blogPost: 'ince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentia',
+        welcomeScreen: true,
+        photos: 'coding'
+      },
+      sampleBlogsPost: [
+        {
+          title: 'sample',
+          blogHTML: 'Edward Joseph Snowden (born June 21, 1983) is a former computer intelligence consultant who leaked highly classified information from the National Security Agency (NSA) in 2013 when he was an employee and subcontractor for the Central Intelligence Agency (CIA).',
+          blogCoverPhoto: 'ed_snowden'
+        },
+        {
+          title: 'sample',
+          blogHTML: 'Abigail Suzanne Martin is an American journalist,[2][3] TV presenter and activist. She helped found the citizen journalism website Media Roots and serves on the board of directors for the Media Freedom Foundation which manages Project Censored.[4][5]',
+          blogCoverPhoto: 'abby_martin'
+        },
+        {
+          title: 'sample',
+          blogHTML: 'Julian Paul Assange (/əˈsɑːnʒ/;[3] born 3 July 1971) is an Australian editor, publisher, and activist who founded WikiLeaks in 2006. WikiLeaks came to international attention in 2010 when it published a series of leaks provided by U.S. Army intelligence analyst Chelsea Manning.',
+          blogCoverPhoto: 'julian_assange'
+        },
+      ],
+    }
+  },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards
+    },
+    user() {
+      return this.$store.state.user;
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media(min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media(min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+
+      @media(min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+
+    }
+  }
+}
+</style>
